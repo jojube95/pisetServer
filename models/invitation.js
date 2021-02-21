34e738b5d908e3979c94ce2d754ef874123fcee4
jojube95 <joanjube95@gmail.com>
2021-02-21 18:13:40 +0100

    Invite user to current group

--- models/invitation.js
+++ models/invitation.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const invitationSchema = mongoose.Schema({
     userId: {type: String, required: true},
-    groupId: {type: String, required: true}
+    groupId: {type: String, required: true},
+    groupName: {type: String, required: true},
 });
 
 module.exports = mongoose.model('Invitation', invitationSchema);
--- routes/invitations.js
+++ routes/invitations.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const MODEL_PATH = '../models/';
 const Invitation = require(MODEL_PATH + 'invitation');
+const User = require(MODEL_PATH + 'user');
 
 const router = express.Router();
 
@@ -10,18 +11,68 @@ module.exports = function(io) {
 };
 
 router.get('/getInvitations', (req, res, next) => {
-
+    Invitation.findOne({ userId: req.params.id }).then(result =>{
+        res.status(200).json({
+            message: "Success",
+            task: result
+        });
+    }).catch(err => {
+        res.status(500).json({
+            error : err
+        })
+    });
 });
 
 router.post('/invite', (req, res, next) => {
+    const invitation = new Invitation({
+        userId: req.body.userId,
+        groupId: req.body.groupId,
+        groupName: req.body.groupName
+    });
 
+    invitation.save().then(result => {
+        res.status(201).json({
+            message: 'Invitation added successfully',
+            result: result
+        });
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.post('/accept', (req, res, next) => {
-
+    User.updateOne(
+        {'_id': req.body.userId}, { $set: { groupId: req.body.groupId, groupName: req.body.groupName }
+    }).then(result => {
+        Invitation.deleteOne({'_id': req.body.id}).then(result => {
+            res.status(201).json({
+                message: 'Invitation accepted successfully',
+                result: result
+            });
+        }).catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.post('/decline', (req, res, next) => {
-
+    Invitation.deleteOne({'_id': req.body.id}).then(result => {
+        res.status(201).json({
+            message: 'Invitation deleted successfully',
+            result: result
+        });
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
