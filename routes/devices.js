f86cb9a85c7e22e3792d9a8a6aeb11f3481953a5
jojube95 <joanjube95@gmail.com>
2021-03-21 20:35:49 +0100

    Change api calls for new data model

--- routes/devices.js
+++ routes/devices.js
@@ -9,5 +9,17 @@ module.exports = function(io) {
     return router;
 };
 
+router.get('/getByUser:id', (req, res, next) => {
+    Device.find({ userId: req.params.id }).then(result =>{
+        res.status(200).json({
+            message: "Success",
+            devices: result
+        });
+    }).catch(err => {
+        res.status(500).json({
+            error : err
+        })
+    });
+});
 
 module.exports = router;
